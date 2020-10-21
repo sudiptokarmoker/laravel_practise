bd83bf4c741ac0bf69101c962c2bb0aed628dfb4
sudipto <sudiptocsi@gmail.com>
2020-10-21 16:01:14 +0600

    added

--- app/Http/Controllers/Backened/Auth/LoginController.php
+++ app/Http/Controllers/Backened/Auth/LoginController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Backened\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = RouteServiceProvider::ADMIN_DASHBOARD;
+
+    public function showLoginForm(Request $request){
+        // show login form
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backened\Auth\ForgotPasswordController;
+use App\Http\Controllers\Backened\Auth\LoginController;
 use App\Http\Controllers\Backened\Dashboard;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
@@ -36,17 +38,21 @@ Route::view('/dashboard/show', [IndexController::class, 'show'])->name('dashboar
 Route::view('/dashboard/create', [IndexController::class, 'create'])->name('dashboard.create')->middleware('auth');
 */
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin'], function () {
     Route::get('/', [Dashboard::class, 'index'])->name('admin.dashboard');
     //Route::get('roles', [RolesController::class, 'index']);
     //Route::get('/roles', [RolesController::class, 'index']);
     //Route::resource('roles', RolesController::class);
     Route::resource('roles', RolesController::class, ['name' => 'admin.roles']);
     Route::resource('users', UsersController::class, ['name' => 'admin.users']);
-
     // Login route
-    Route::get('/login', );
-
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->mame('admin.login');
+    Route::post('/login/submit', [LoginController::class, 'login'])->name('admin.login.submit');
+    // Logout route
+    Route::post('/logout/submit', [LoginController::class, 'logout'])->name('admin.logout.submit');
+    // forot password route
+    Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('admin.password.request');
+    Route::get('/password/reset/submit', [ForgotPasswordController::class, 'reset'])->name('admin.password.update');
 });
 
 Route::get('/test', [TestController::class, 'index']);
